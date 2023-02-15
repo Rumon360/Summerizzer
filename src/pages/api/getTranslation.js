1a999d2419b703d1906032dede1aea0fdfbfbadc
Rumon360 <hmk.rumon@gmail.com>
2023-02-16 01:44:21 +0600

    killme

--- src/pages/api/getTranslation.js
+++ src/pages/api/getTranslation.js
@@ -4,10 +4,14 @@ const { translate } = require("free-translate");
 export default async function handler(req, res) {
   const { text, lang } = req.body;
 
-  const translatedText = await translate(text, {
-    from: "en",
-    to: lang,
-  });
+  try {
+    const translatedText = await translate(text, {
+      from: "en",
+      to: lang,
+    });
 
-  res.status(200).json({ data: translatedText });
+    res.status(200).json({ data: translatedText });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 }
